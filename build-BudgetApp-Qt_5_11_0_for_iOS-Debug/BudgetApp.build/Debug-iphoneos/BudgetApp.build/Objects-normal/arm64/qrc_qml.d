3ca10560d93dca2a8cb206216192c5b26cefaa2f
Matthew Magni <mmagni@uwaterloo.ca>
2018-06-23 15:44:29 -0400

    remove build directories

--- build-BudgetApp-Qt_5_11_0_for_iOS-Debug/BudgetApp.build/Debug-iphoneos/BudgetApp.build/Objects-normal/arm64/qrc_qml.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/matthewmagni/Documents/Development/iOS/Qt/BudgetAppTop/build-BudgetApp-Qt_5_11_0_for_iOS-Debug/qrc_qml.cpp
--- build-BudgetApp-Qt_5_11_0_for_iOS-Debug/BudgetApp.build/Debug-iphoneos/Qt Preprocess.build/Script-7EF0942E79C014DCEC8976BC.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/matthewmagni/Documents/Development/iOS/Qt/BudgetAppTop/build-BudgetApp-Qt_5_11_0_for_iOS-Debug/ -f BudgetApp.xcodeproj/qt_preprocess.mak
